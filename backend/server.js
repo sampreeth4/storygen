ee1f50ca74dffda3ef0338236edc50eb478eb4dd
sampreeth <saisampreeth78@gmail.com>
2025-03-02 20:02:22 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Story Generator API");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
